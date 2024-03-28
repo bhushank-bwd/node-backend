e7616b32303d118f4ccc40486145e226e2510bce
bhushanK <bhushank@noitavonne.com>
2024-03-28 20:10:08 +0530

    Mail sent using nodemailer

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import express, { json } from "express";
 import cors from "cors";
 import { config as dotenvConfig } from "dotenv";
 import connectToMongoDB from "./database/db_connection.js";
+import { readFileContent } from "./helper/file_helper.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { sendHtmlMail } from "./helper/email_helper.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 dotenvConfig();
 
 connectToMongoDB();
@@ -10,8 +17,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(json());
 
-app.get("/", (req, res) => {
-  res.json("Hello, this is the root route!");
+app.get("/", async (req, res) => {
+  const html = await readFileContent(
+    __dirname + "/assets/html/email_templates/welcome.html"
+  );
+  const emailRes = await sendHtmlMail(html);
+  if (emailRes) {
+    res.json("Mail sent successfully!");
+  } else {
+    res.json("Problem in mail sending!");
+  }
 });
 
 app.listen(port, () => {

8636fac91e1a1c4770031a1a24b5305c5937112d
bhushanK <bhushank@noitavonne.com>
2024-03-29 20:32:23 +0530

    csv and xlsx file read using xlsx package

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import express, { json } from "express";
 import cors from "cors";
 import { config as dotenvConfig } from "dotenv";
 import connectToMongoDB from "./database/db_connection.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { readXLSXData } from "./helper/xlsx_helper.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 dotenvConfig();
 
 connectToMongoDB();
@@ -13,7 +19,22 @@ app.use(json());
 app.get("/", (req, res) => {
   res.json("Hello, this is the root route!");
 });
-
+app.get("/read-xlsx", async (req, res) => {
+  let readXlsxRes = await readXLSXData(__dirname + "/assets/xlsx/sample3.xlsx");
+  if (readXlsxRes) {
+    res.json(readXlsxRes);
+  } else {
+    res.json("No file data available");
+  }
+});
+app.get("/read-csv", async (req, res) => {
+  let readXlsxRes = await readXLSXData(__dirname + "/assets/csv/machine.csv");
+  if (readXlsxRes) {
+    res.json(readXlsxRes);
+  } else {
+    res.json("No file data available");
+  }
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
